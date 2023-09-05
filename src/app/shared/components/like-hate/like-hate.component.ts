284ab6e4eb53467264638759fec6d0d04a8ee12b
Thomas Quinton <tquinton@kaliop.com>
2023-09-05 19:09:30 +0200

    anim

--- src/app/shared/components/like-hate/like-hate.component.ts
+++ src/app/shared/components/like-hate/like-hate.component.ts
@@ -8,11 +8,21 @@ import { Component, Output, EventEmitter } from '@angular/core';
 export class LikeHateComponent {
   @Output() likeHate:EventEmitter<number> = new EventEmitter<number>();
 
+  pouet = 0;
+
   handleLikeClick() {
     this.likeHate.emit(1)
+    this.pouet += 1
+    setTimeout(() => {
+      this.pouet = 0
+    }, 150);
   }
 
   handleHateClick() {
     this.likeHate.emit(-1)
+    this.pouet -= 1
+    setTimeout(() => {
+      this.pouet = 0
+    }, 150);
   }
 }
