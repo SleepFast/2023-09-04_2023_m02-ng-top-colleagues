9e93897ceee27e63fe5aa0b9fd7252770f4e6a8f
Thomas Quinton <tquinton@kaliop.com>
2023-09-14 23:39:35 +0200

    fix menu

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,36 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss']
+  styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {
-  welcomePage = true
-  template = false
-  reactive = false
+export class MenuComponent implements OnInit{
+  welcomePage = true;
+  template = false;
+  reactive = false;
 
+  constructor(private location: Location) {}
+
+
+  ngOnInit(): void {
+    switch (this.location.prepareExternalUrl(this.location.path())) {
+      case '/welcomePage':
+        this.welcomePage = true;
+        this.template = false;
+        this.reactive = false;
+        break;
+      case '/template':
+        this.welcomePage = false;
+        this.template = true;
+        this.reactive = false;
+        break;
+      case '/reactive':
+        this.welcomePage = false;
+        this.template = false;
+        this.reactive = true;
+        break;
+      default:
+        this.welcomePage = true;
+        this.template = false;
+        this.reactive = false;
+    }
+  }
   handleLinkClick(link: string) {
     switch (link) {
       case 'welcomePage':
-        this.welcomePage = true
-        this.template = false
-        this.reactive = false
+        this.welcomePage = true;
+        this.template = false;
+        this.reactive = false;
         break;
       case 'template':
-        this.welcomePage = false
-        this.template = true
-        this.reactive = false
+        this.welcomePage = false;
+        this.template = true;
+        this.reactive = false;
         break;
       case 'reactive':
-        this.welcomePage = false
-        this.template = false
-        this.reactive = true
+        this.welcomePage = false;
+        this.template = false;
+        this.reactive = true;
         break;
       default:
-        this.welcomePage = true
-        this.template = false
-        this.reactive = false
+        this.welcomePage = true;
+        this.template = false;
+        this.reactive = false;
     }
   }
 }
